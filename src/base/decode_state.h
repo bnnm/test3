c233c47dabae1a843d8ecfdfe7984f8dcd7580a1
bnnm <bananaman255@gmail.com>
2025-05-12 18:50:08 +0200

    test

---
+++ src/base/decode_state.h
@@ -0,0 +1,12 @@
+#ifndef _DECODE_STATE_H
+#define _DECODE_STATE_H
+
+#include "sbuf.h"
+
+typedef struct {
+    int discard;
+    sbuf_t sbuf;
+    int samples_left; //info for some decoders
+} decode_state_t;
+
+#endif
